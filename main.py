f53ad7759431cdfa44eaf310bd7fd5db5a42e3c2
Animaznman <37084674+Animaznman@users.noreply.github.com>
2022-11-30 12:08:05 -0800

    Added update_character_options function
    This function is for updating the reference used for character options.

--- main.py
+++ main.py
@@ -55,6 +55,46 @@ def html_output(character):
 
     put_button('Generate Character', onclick = button_clicked)
 
+def update_character_options():
+
+    api_key = 'da468b89-2bf8-4e2b-a939-79c6e6ef25ce'
+    header = {"Authorization": api_key}
+    r = requests.get('https://api.pathfinder2.fr/v1/pf2', headers = header)
+    print(r.status_code)
+
+    # Update list of ancestries
+    ancestries = requests.get('https://api.pathfinder2.fr/v1/pf2/ancestry', headers = header)
+    ancestry_list = []
+    for result in ancestries.json()['results']:
+        ancestry_list.append(result['name'])
+    ancestry_list.pop()
+
+    # Update list of backgrounds
+    backgrounds = requests.get('https://api.pathfinder2.fr/v1/pf2/background', headers = header)
+    background_list = []
+    for result in backgrounds.json()['results']:
+        background_list.append(result['name'])
+    background_list.pop()
+
+    # Update list of classes
+    classes = requests.get('https://api.pathfinder2.fr/v1/pf2/class', headers = header)
+    classes.json()['results']
+    class_list = []
+    for result in classes.json()['results']:
+        class_list.append(result['name'])
+    class_list.pop()
+
+    # Produce csv for character_options.csv
+    max_len = max([len(ancestry_list), len(background_list), len(class_list)])
+    for item in [ancestry_list, background_list, class_list]:
+        while len(item)< max_len:
+            item.append(None)
+
+
+    data = {'Ancestries': ancestry_list, 'Backgrounds': background_list, 'Classes': class_list}
+
+    df = pd.DataFrame(data = data)
+    df.to_csv('character_options.csv')
     
 
 
